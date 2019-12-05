124d72f0dbc877d872ee2fbf5f21cff4362ba181
ronnie88597 <897045953@qq.com>
2019-12-05 09:21:37 +0800

    add

---
+++ llvm/llvm_cookbook/example/chapter_03_self/cmake-build-debug/lexer/CMakeFiles/lex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../lib/liblex.a"
+  "../lib/liblex.pdb"
+  "CMakeFiles/lex.dir/lex.yy.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ llvm/llvm_cookbook/example/chapter_03_self/cmake-build-debug/unittest/lexer_test/CMakeFiles/lexer_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../bin_test/lexer_test"
+  "../../bin_test/lexer_test.pdb"
+  "CMakeFiles/lexer_test.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lexer_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
